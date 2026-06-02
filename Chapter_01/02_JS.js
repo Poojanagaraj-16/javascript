65b76d95fb8f1d649184b65f2f2014946e894406
Poojanagaraj-16 <bnpooja1234@gmail.com>
2026-06-02 10:58:24 +0530

    Add Chapter_01 and chapter_02 content

---
+++ Chapter_01/02_JS.js
@@ -0,0 +1,20 @@
+let a = 10;
+console.log(a);
+
+
+for (let a = 0; a < 10; a++) {
+    console.log(a);
+    print()
+    print1()
+
+}
+
+function print() {
+    console.log("Hello");
+
+
+}
+
+function print1() {
+    console.log("hi")
+}
